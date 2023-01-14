6f4bae4f5bca027620ae20995f2204bcb90d2946
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-15 00:01:11 +0530

    created dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import albumentations
+import torch
+from torch.utils.data import Dataset
+
+from tqdm import tqdm
+
+
+class MelanomaDataset(Dataset):
+    def __init__(self, df, meta_features=None, transforms=None):
+        self.df = df.reset_index(drop=True)
+        self.meta_features = meta_features
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        img_name = self.df.iloc[index].image_name
+        img_path = os.path.join('../output/train', img_name + '.jpg')
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transforms is not None:
+            res = self.transforms(image=image)
+            image = res['image'].astype(np.float32)
+        else:
+            image = image.astype(np.float32)
+
+        image = image.transpose(2, 0, 1)
+
+        if self.meta_features is not None:
+            img = torch.tensor(image).float()
+            meta_feats = torch.tensor(
+                self.df.iloc[index][self.meta_features]
+            ).float()
+        else:
+            img = torch.tensor(image).float()
+            meta_feats = None
+        return {
+            'image': img,
+            'meta_features': meta_feats,
+            'target': torch.tensor(self.df.iloc[index].target).long()
+        }
