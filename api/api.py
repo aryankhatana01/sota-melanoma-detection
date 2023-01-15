6233fd409c4ac5cef7680538bf5d3c10f5c57c90
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-15 21:33:00 +0530

    sample api code working

---
+++ api/api.py
@@ -0,0 +1,30 @@
+"""
+FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints.
+To Run the Server:
+uvicorn api:app --reload
+"""
+
+from typing import Union, List
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+import utils
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
