a6731cc6e16b50f39e97a722011a1a35a65ed784
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-15 12:37:50 +0530

    added utils for training

---
+++ src/utils.py
@@ -0,0 +1,72 @@
+import albumentations
+import numpy as np
+import pandas as pd
+import os
+from tqdm import tqdm
+
+# Function to get Image transforms
+def get_transforms(image_size=512):
+    """
+    Returns a tuple of train and validation transforms
+    """
+    transforms_train = albumentations.Compose([
+        albumentations.Transpose(p=0.5),
+        albumentations.VerticalFlip(p=0.5),
+        albumentations.HorizontalFlip(p=0.5),
+        albumentations.RandomBrightness(limit=0.2, p=0.75),
+        albumentations.RandomContrast(limit=0.2, p=0.75),
+        albumentations.OneOf([
+            albumentations.MotionBlur(blur_limit=5),
+            albumentations.MedianBlur(blur_limit=5),
+            albumentations.GaussianBlur(blur_limit=5),
+            albumentations.GaussNoise(var_limit=(5.0, 30.0)),
+        ], p=0.7),
+
+        albumentations.OneOf([
+            albumentations.OpticalDistortion(distort_limit=1.0),
+            albumentations.GridDistortion(num_steps=5, distort_limit=1.),
+            albumentations.ElasticTransform(alpha=3),
+        ], p=0.7),
+
+        albumentations.CLAHE(clip_limit=4.0, p=0.7),
+        albumentations.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.5),
+        albumentations.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=0.85),
+        albumentations.Resize(image_size, image_size),
+        albumentations.Cutout(max_h_size=int(image_size * 0.375), max_w_size=int(image_size * 0.375), num_holes=1, p=0.7),
+        albumentations.Normalize()
+    ])
+
+    transforms_val = albumentations.Compose([
+        albumentations.Resize(image_size, image_size),
+        albumentations.Normalize()
+    ])
+
+    return transforms_train, transforms_val
+
+def get_meta_data(df_train):
+
+    # One-hot encoding of anatom_site_general_challenge feature
+    dummies = pd.get_dummies(df_train['anatom_site_general_challenge'], dummy_na=True, dtype=np.uint8, prefix='site')
+    df_train = pd.concat([df_train, dummies.iloc[:df_train.shape[0]]], axis=1)
+    # Sex features
+    df_train['sex'] = df_train['sex'].map({'male': 1, 'female': 0})
+    df_train['sex'] = df_train['sex'].fillna(-1)
+    # Age features
+    df_train['age_approx'] /= 90
+    df_train['age_approx'] = df_train['age_approx'].fillna(0)
+    df_train['patient_id'] = df_train['patient_id'].fillna(0)
+    # n_image per user
+    df_train['n_images'] = df_train.patient_id.map(df_train.groupby(['patient_id']).image_name.count())
+    df_train.loc[df_train['patient_id'] == -1, 'n_images'] = 1
+    df_train['n_images'] = np.log1p(df_train['n_images'].values)
+    # image size
+    train_images = df_train['filepath'].values
+    train_sizes = np.zeros(train_images.shape[0])
+    for i, img_path in enumerate(tqdm(train_images)):
+        train_sizes[i] = os.path.getsize(img_path)
+    df_train['image_size'] = np.log(train_sizes)
+
+    meta_features = ['sex', 'age_approx', 'n_images', 'image_size'] + [col for col in df_train.columns if col.startswith('site_')]
+    n_meta_features = len(meta_features)
+
+    return df_train, meta_features, n_meta_features
