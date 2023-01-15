d7878ebe2b196119d33b8f6d6bcace68ba0ac7be
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-15 12:52:51 +0530

    added a warmup scheduler

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 from tqdm import tqdm
+from warmup_scheduler import GradualWarmupScheduler  # https://github.com/ildoonet/pytorch-gradual-warmup-lr
+
 
 # Function to get Image transforms
 def get_transforms(image_size=512):
@@ -74,3 +76,20 @@ def get_meta_data(df_train):
     n_meta_features = len(meta_features)
 
     return df_train, meta_features, n_meta_features
+
+
+class GradualWarmupSchedulerV2(GradualWarmupScheduler):
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        super(GradualWarmupSchedulerV2, self).__init__(optimizer, multiplier, total_epoch, after_scheduler)
+    def get_lr(self):
+        if self.last_epoch > self.total_epoch:
+            if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
+                return self.after_scheduler.get_lr()
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+        if self.multiplier == 1.0:
+            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+        else:
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
