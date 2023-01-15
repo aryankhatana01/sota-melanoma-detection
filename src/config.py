283aab64ae29634e4c6d0d0454ab31f75f09d30a
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-15 14:33:47 +0530

    config created

---
+++ src/config.py
@@ -0,0 +1,35 @@
+import torch
+
+class Config:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    num_workers = 4
+    batch_size = 16
+    image_size = 512
+    n_epochs = 10
+    lr = 1e-4
+    use_meta = True
+    use_scheduler = True
+    scheduler = 'CosineAnnealingLR'
+    scheduler_params = dict(
+        T_max=10,
+        eta_min=1e-6,
+        last_epoch=-1,
+    )
+    warmup_epochs = 1
+    warmup_factor = 10
+    warmup_method = 'linear'
+    n_fold = 5
+    seed = 42
+    model_name = 'tf_efficientnet_b6_ns'
+    model_params = dict(
+        pretrained=True,
+        drop_rate=0.5,
+        drop_path_rate=0.2,
+    )
+    criterion = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam
+    optimizer_params = dict(
+        lr=lr,
+        weight_decay=1e-6,
+    )
+    verbose = True
