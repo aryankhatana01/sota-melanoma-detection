73e8708ea2960bc4be6970ae9d1593532b7fdcc7
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-17 18:03:27 +0530

    frontend doneˇ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
-// import Footer from './components/footer/Footer';
+import Footer from './components/footer/Footer';
+import PredictButton from './components/predictButton/PredictButton';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <div className="App">
         <Header />
         <Main />
+        <PredictButton />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/predictButton/PredictButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './PredictButton.css';
+
+const PredictButton = () => {
+    return (
+        <div className="predict-button">
+            <button className="predict-button-button">Predict!</button>
+        </div>
+    )
+}
+
+export default PredictButton;
