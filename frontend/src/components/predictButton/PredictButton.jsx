22c2cfb11e1537e540bd5608b9f4e1560718c2d5
aryankhatana01 <aryankhatana01@gmail.com>
2023-01-18 13:57:52 +0530

    Prediction Render Completed✅

--- frontend/src/components/predictButton/PredictButton.jsx
+++ frontend/src/components/predictButton/PredictButton.jsx
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './PredictButton.css';
 import FileContext from "../contexts/FileContext";
 
 
 const PredictButton = () => {
     const selectedFile = useContext(FileContext);
+    const [pred, setPred] = useState(null);
 
     const handleUpload = async () => {
         const formData = new FormData();
@@ -26,11 +27,19 @@ const PredictButton = () => {
         };
         const resp = await fetch(`http://localhost:8000/predict/?filename=${selectedFile.name}`, requestOptions);
         const data = await resp.json();
+        setPred(data["Prediction"]);
         console.log(data);
     }
     return (
         <div className="predict-button">
             <button className="predict-button-button" onClick={handlePredict}>Predict!</button>
+            {/* {Object.entries(pred).map(([key, value]) => (
+                <div key={key}>{key}: {value}</div>
+            ))} */}
+            <div className="pred">
+                {/* {pred} */}
+                {pred===null ? null : (pred === 6 ? <div className="detected">Prediction: Melanoma Detected</div> : <div className="no-detected">Prediction: No Melanoma Detected</div>)}
+            </div>
             {/* <div className="">
                 {selectedFile}
             </div> */}
